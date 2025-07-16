7977ceb3da0a9f76f8e29f2a0e51223dbbf7e718
bluuas <bluuas@proton.me>
2025-07-16 15:39:34 +0200

    add the initial setup to the nuke

--- nuke_inventree.sh
+++ nuke_inventree.sh
@@ -17,11 +17,28 @@ reset_inventree_database() {
     echo "Resetting the InvenTree database..."
 
     # Echo 'y' to confirm deletion and run the command to delete all database records
-    echo "y" | docker-compose -f $COMPOSE_FILE run --rm inventree-server invoke dev.delete-data
+    echo "yes" | docker-compose -f $COMPOSE_FILE run --rm inventree-server invoke dev.delete-data
 
     echo "InvenTree database has been reset."
     echo "(Note: InvenTree API key has also been deleted)"
 }
 
+# Function to set up a new InvenTree instance
+setup_inventree() {
+    echo "Setting up a new InvenTree instance..."
+
+    # Run initial database setup using docker compose
+    docker compose -f $COMPOSE_FILE run --rm inventree-server invoke update
+
+    # Create administrator account with multiple inputs
+    # docker compose -f $COMPOSE_FILE run inventree-server invoke superuser              
+
+    # Start the InvenTree container
+    # docker-compose -f $COMPOSE_FILE up -d
+
+    echo "InvenTree setup completed."
+}
+
 # Main script execution
 reset_inventree_database
+setup_inventree
