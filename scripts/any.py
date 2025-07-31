90008cb0ac6cdebb2c51df7f1b7d5ffa4f5b426f
bluuas <bluuas@proton.me>
2025-07-31 13:35:41 +0200

    modify parser arguments

--- scripts/any.py
+++ scripts/any.py
@@ -6,7 +6,8 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(description="InvenTree CLI")
-    parser.add_argument('-d', '--delete-all', action='store_true', help='Delete all entities')
+    parser.add_argument('--delete-all', action='store_true', help='Delete all entities')
+    parser.add_argument('-d', '--directory', required=True, help='Directory containing CSV files')
 
     load_dotenv()
 
@@ -19,15 +20,15 @@ def main():
 
     if args.delete_all:
         utils.delete_all(api)
-        return  # Finish after deleting all entities
+        return
 
-    # install the KiCad Plugin for InvenTree
+    # Install the KiCad Plugin for InvenTree
     utils.configure_inventree_plugin_settings(api)
     utils.install_and_activate_kicad_plugin(api)
 
     # Get the directory of the current script
     script_dir = os.path.dirname(os.path.abspath(__file__))
-    csv_source_dir = os.path.join(script_dir, 'csv-database')
+    csv_source_dir = os.path.join(script_dir, args.directory)
 
     # Process CSV files in the specified directory
     for filename in os.listdir(csv_source_dir):
@@ -35,6 +36,6 @@ def main():
             utils.process_csv_file(api, os.path.join(csv_source_dir, filename))
 
     utils.update_kicad_plugin_settings(api)
-    
+
 if __name__ == "__main__":
     main()
